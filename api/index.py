da1e7c70589e4512f1c4c03f168be9eb289727f3
David Hodge <davidhodge@Davids-MacBook-Air.local>
2026-02-18 15:58:26 +0800

    Remove debug traceback wrapper from api entry point

--- api/index.py
+++ api/index.py
@@ -1,7 +1,2 @@
-import traceback, sys
-try:
-    from app import create_app
-    app = create_app()
-except Exception:
-    traceback.print_exc(file=sys.stderr)
-    raise
+from app import create_app
+app = create_app()
